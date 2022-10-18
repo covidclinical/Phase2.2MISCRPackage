846043571859b763d9c99631f13c68f465a98678
Simran Makwana <simranmakw@gmail.com>
2022-10-18 20:55:08 +0000

    Add variables with no patients to table3

--- FourCePhase2.2MISC/R/misc_table3.R
+++ FourCePhase2.2MISC/R/misc_table3.R
@@ -98,12 +98,7 @@ misc_table3 <- function(complete_df, obfuscation_threshold, currSiteId, dir.outp
     group_by( categories ) %>%
     mutate( p.value =round( fisher.test( value, variant_misc )$p.value, 3) ) %>%
     select( categories, p.value ) %>%
-    unique() %>%
-    ############## not working as expected
-    # add in the variables with no patients as 0
-    ungroup() %>%
-    add_row(categories = colnames(mainTable)[!colnames(mainTable) %in% outcomes],
-            p.value = NA)
+    unique()
 
   ## estimate the N and percentage per category
   counts_percentages <- long_table %>%
@@ -137,6 +132,23 @@ misc_table3 <- function(complete_df, obfuscation_threshold, currSiteId, dir.outp
     unique() %>%
     tidyr::pivot_wider(names_from = variant_misc, values_from = n_perc, values_fill = '0 (0%)') %>%
     left_join( fisherTest )
+
+  # order rows
+  ordered_rows <- outcomes
+
+  # reorder combined df
+  output_table3_with_stats <- output_table3_with_stats %>%
+    # this ensures the variables which were not able to be done with the fisher test are included
+    right_join(data.frame('categories' = ordered_rows)) %>%
+    filter( categories %in% ordered_rows ) %>%
+    mutate(categories =  factor(categories, levels = ordered_rows)) %>%
+    arrange(categories)
+
+  # change NA by 0 (0%)
+  output_table3_with_stats[ is.na( output_table3_with_stats$Alpha ), ]$Alpha <- "0 (0%)"
+  output_table3_with_stats[ is.na( output_table3_with_stats$Delta ), ]$Delta <- "0 (0%)"
+  output_table3_with_stats[ is.na( output_table3_with_stats$Omicron ), ]$Omicron <- "0 (0%)"
+  output_table3_with_stats[ is.na( output_table3_with_stats$total ), ]$total <- "0 (0%)"
 
 
   # export table
