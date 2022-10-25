5ac1fded23c42716394edd53e9bca715ae573c46
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2022-10-25 13:53:37 +0000

    Add code to test edge cases in generating table summaries

--- test_runAnalysis.R
+++ test_runAnalysis.R
@@ -20,3 +20,25 @@ FourCePhase2.2MISC::runAnalysis( dir.input = "/4ceData/Input/4CE_MISC_15SEP2022_
 
 ###
 
+
+# code to test edge cases
+
+head(misc_complete)
+misc_complete_original <- misc_complete
+
+# test if a site does not have 3 variants
+misc_complete <- misc_complete_original
+misc_complete <- misc_complete %>% filter(variant_misc == "Alpha")
+
+
+# test if a site does not have 3 variants
+# if there is at least 2, run the statistical test, implement this test case
+misc_complete_original <- misc_complete
+misc_complete <- misc_complete %>% filter(variant_misc %in% c("Alpha", "Delta"))
+
+
+# test if there are labs with no values for certain variants
+# example: 1751-7 (albumin)
+misc_complete <- misc_complete_original
+misc_complete[misc_complete$concept_code == '1751-7' & misc_complete$variant_misc == 'Delta', "value"] <- 99999
+misc_complete <- misc_complete %>% filter(value != 99999)
