8c242e06f33808ede493fe87b240be9b456a1654
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2023-03-24 16:56:52 -0400

    adapt the code to run meta-analysis with all the clinical characteristics

--- MetaAnalysis/metaMISC.R
+++ MetaAnalysis/metaMISC.R
@@ -31,9 +31,6 @@ ft.inverse = function(x){
   (exp(2*x)-1)/(exp(2*x)+1)
 }
 
-diag_list = c("CARDIOVASCULAR SYMPTOMS","COVID-19","GI SYMPTOMS",
-              "LIVER DYSFUNCTION","NEUROLOGIC SYMPTOMS","RENAL DYSFUNCTION",
-              "RESPIRATORY SYMPTOMS","SHOCK/SIRS")
 data_allsite = vector("list",length(site_list))
 for (i in 1:length(site_list)){
   
@@ -50,7 +47,12 @@ for (i in 1:length(site_list)){
   data_allsite[[i]] = count_wide
 }
 names(data_allsite) = site_list
-dim(data_allsite$BCH)
+
+diag_list = c("CARDIOVASCULAR SYMPTOMS","COVID-19","GI SYMPTOMS",
+              "LIVER DYSFUNCTION","NEUROLOGIC SYMPTOMS","RENAL DYSFUNCTION",
+              "RESPIRATORY SYMPTOMS","SHOCK/SIRS")
+#diag_list = data_allsite[[1]]$categories
+
 
 data_alldiag = vector("list",length(diag_list))
 for (i in 1:length(diag_list)){
@@ -79,6 +81,7 @@ names(data_alldiag) = diag_list
 
 res.exact.delta = vector("list",length(diag_list))
 for (i in 1:length(diag_list)){
+  print(paste0("#####", i))
   dat.exact = cbind(data_alldiag[[i]][,1:2],sample_size[,1:2])
   res.exact = meta.exact(dat.exact, type="risk difference")
   res.exact.delta[[i]] = res.exact
@@ -86,16 +89,22 @@ for (i in 1:length(diag_list)){
 
 res.exact.omicron = vector("list",length(diag_list))
 for (i in 1:length(diag_list)){
+  print(paste0("#####", i))
   dat.exact = cbind(data_alldiag[[i]][,c(1,3)],sample_size[,c(1,3)])
   res.exact = meta.exact(dat.exact, type="risk difference")
   res.exact.omicron[[i]] = res.exact
 }
 
+#save(res.exact.delta,res.exact.omicron,file="exactresult_all.RData")
 save(res.exact.delta,res.exact.omicron,file="exactresult.RData")
 
 #############################
 ### approach II: exact CI ###
 #############################
+load("exactresult_all.RData")
+load("exactresult.RData")
+library(dplyr)
+
 delta.exact = NULL
 for (i in 1:length(res.exact.delta)){
   delta.exact = rbind(delta.exact,res.exact.delta[[i]]$ci.fixed[,2])
@@ -120,7 +129,9 @@ print( omicron.exact %>% filter( p < 0.01 ))
 delta.hybrid = NULL
 transform = FALSE
 
-for (i in 1:length(res.exact.delta)){
+#for (i in 1:length(res.exact.delta)){
+for (i in c(1:15,17:length(res.exact.delta))){
+  print(i)
   yi = NULL
   yi.alt = NULL
   vi = NULL
@@ -147,13 +158,16 @@ for (i in 1:length(res.exact.delta)){
 }
 delta.hybrid = data.frame(delta.hybrid)
 colnames(delta.hybrid) = c("est","ci.lb","ci.ub","pval")
-delta.hybrid$outcome = diag_list
+delta.hybrid$outcome = diag_list[-16]
 
 print( delta.hybrid %>% filter( pval < 0.05))
 
 
 omicron.hybrid = NULL
-for (i in 1:length(res.exact.omicron)){
+#for (i in 1:length(res.exact.omicron)){
+for (i in c(1:67,69:length(res.exact.omicron))){
+  
+  print(i)
   yi = NULL
   yi.alt = NULL
   vi = NULL
@@ -180,7 +194,7 @@ for (i in 1:length(res.exact.omicron)){
 }
 omicron.hybrid = data.frame(omicron.hybrid)
 colnames(omicron.hybrid) = c("est","ci.lb","ci.ub","pval")
-omicron.hybrid$outcome = diag_list
+omicron.hybrid$outcome = diag_list[-68]
 print( omicron.hybrid %>% filter( pval < 0.05))
 
 ################################
@@ -268,6 +282,103 @@ for( j in 1:length(lab_names_list)){
     
   }
 
+  labs_results$lab[j] <- this_lab
+  labs_results$alpha_delta_pval[j] <- stats_output_alpha_delta$pval
+  labs_results$alpha_omicron_pval[j] <- stats_output_alpha_omicron$pval
+  
+  if( stats_output_alpha_delta$pval < 0.05){
+    outputs_to_plot[[this_lab]] <- stats_output_alpha_delta
+  }
+  if( stats_output_alpha_omicron$pval < 0.05){
+    outputs_to_plot[[this_lab]] <- stats_output_alpha_omicron
+  }
+  #labs_results$alpha_delta_pval_ee[j] <- stats_output_alpha_delta_ee$pval
+  #labs_results$alpha_omicron_pval_ee[j] <- stats_output_alpha_omicron_ee$pval
+}
+
+forest( outputs_to_plot[[1]])
+forest( outputs_to_plot[[2]])
+
+###### During admission
+# meta analysis of two-sample t-test
+lab_bysite = vector("list",length(site_list))
+for (i in 1:length(site_list)){
+  print(i)
+  filename = paste(site_list[i], "/replace_earlier/",site_list[i],"_table2DuringAdmission.RData",sep="")
+  load(filename)
+  
+  table2_during = table2_during %>% filter( ! is.na( median_value ))
+  mean_sd = data.frame(table2_during[,c("variant_misc","variableName","mean_value","sd_value","n_patients")])
+  mean_sd_reshape = reshape(mean_sd,direction="wide",idvar="variableName",timevar="variant_misc")
+  lab_bysite[[i]] = mean_sd_reshape
+}
+for (i in 1:length(site_list)){
+  print(dim(lab_bysite[[i]]))
+}
+
+lab_names_list <- lab_bysite[[1]]$variableName
+#this_lab = "albumin"
+#labs_results <- as.data.frame( matrix( ncol = 5, nrow = length(lab_names_list)))
+#colnames( labs_results) <- c("lab", "alpha_delta_pval", "alpha_omicron_pval",  "alpha_delta_pval_ee", "alpha_omicron_pval_ee")
+labs_results <- as.data.frame( matrix( ncol = 3, nrow = length(lab_names_list)))
+colnames( labs_results) <- c("lab", "alpha_delta_pval", "alpha_omicron_pval")
+
+outputs_to_plot <- list()
+
+for( j in 1:length(lab_names_list)){
+  
+  this_lab = lab_names_list[j]
+  print(this_lab)
+  
+  # all sites measured albumin, we take albumin as an example
+  diff_alpha_delta = rep(0,length(site_list))
+  var.diff_alpha_delta = rep(0,length(site_list))
+  
+  diff_alpha_omicron = rep(0,length(site_list))
+  var.diff_alpha_omicron = rep(0,length(site_list))
+  
+  thres = 0
+  sites.exclude = NULL
+  for (i in 1:length(site_list)){
+    
+    #print(i)
+    index = which(lab_bysite[[i]]$variableName == this_lab)
+    
+    if( !any(index) ){
+      sites.exclude = c(sites.exclude,i)
+    }else{
+      min.size = min(lab_bysite[[i]]$n_patients.Alpha[index],lab_bysite[[i]]$n_patients.Delta[index],lab_bysite[[i]]$n_patients.Omicron[index])
+      if (min.size < thres | is.na(min.size)){
+        sites.exclude = c(sites.exclude,i)
+      }
+      diff_alpha_delta[i] = lab_bysite[[i]]$mean_value.Delta[index] - lab_bysite[[i]]$mean_value.Alpha[index]
+      var.diff_alpha_delta[i] = lab_bysite[[i]]$sd_value.Delta[index]^2/sample_size[i,2] + lab_bysite[[i]]$sd_value.Alpha[index]^2/sample_size[i,1]
+      
+      diff_alpha_omicron[i] = lab_bysite[[i]]$mean_value.Omicron[index] - lab_bysite[[i]]$mean_value.Alpha[index]
+      var.diff_alpha_omicron[i] = lab_bysite[[i]]$sd_value.Omicron[index]^2/sample_size[i,3] + lab_bysite[[i]]$sd_value.Alpha[index]^2/sample_size[i,1]
+      
+    }
+  }
+  
+  set.seed(123)
+  #rma(diff[-sites.exclude], var.diff[-sites.exclude], method="EE")
+  if( is.null(sites.exclude )){
+    stats_output_alpha_delta <- rma(diff_alpha_delta, var.diff_alpha_delta)
+    stats_output_alpha_omicron <- rma(diff_alpha_omicron, var.diff_alpha_omicron)
+    
+    #stats_output_alpha_delta_ee <- rma(diff_alpha_delta, var.diff_alpha_delta, method="EE")
+    #stats_output_alpha_omicron_ee <- rma(diff_alpha_omicron, var.diff_alpha_omicron, method="EE")
+    
+    #stats_output <- rma(diff, var.diff, method="EE")
+  }else{
+    stats_output_alpha_delta  <- rma(diff_alpha_delta[-sites.exclude], var.diff_alpha_delta[-sites.exclude])
+    stats_output_alpha_omicron  <- rma(diff_alpha_omicron[-sites.exclude], var.diff_alpha_omicron[-sites.exclude])
+    
+    #stats_output_alpha_delta_ee  <- rma(diff_alpha_delta[-sites.exclude], var.diff_alpha_delta[-sites.exclude], method="EE")
+    #stats_output_alpha_omicron_ee  <- rma(diff_alpha_omicron[-sites.exclude], var.diff_alpha_omicron[-sites.exclude], method="EE")
+    
+  }
+  
   labs_results$lab[j] <- this_lab
   labs_results$alpha_delta_pval[j] <- stats_output_alpha_delta$pval
   labs_results$alpha_omicron_pval[j] <- stats_output_alpha_omicron$pval
